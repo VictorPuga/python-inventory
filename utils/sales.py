de6d1b60ad881393a9a105cb4fc176d94576354a
VictorPuga <victorpugaruiz@live.com.mx>
2020-10-16 18:28:49 -0600

    add sales and employees csv's

---
+++ utils/sales.py
@@ -0,0 +1,14 @@
+
+def get_sales():
+    columns = (
+        'id',
+        'date',
+        'total_price',
+        'num_products',
+        'product_id',
+        'employee_id'
+    )
+
+    employees = ()
+
+    return []
