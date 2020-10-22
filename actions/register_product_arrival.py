539c7682b6c84efe534dcfff4755b7daf502dbfa
Eva Perez <evastilinski@icloud.com>
2020-10-21 19:34:03 -0600

    update register product arrival

--- actions/register_product_arrival.py
+++ actions/register_product_arrival.py
@@ -8,20 +8,20 @@ from utils import (
 
 def register_product_arrival():
     print("--- Register product arrival ---\n")
-    print("Which product is it?")
+    print("Which product is it?\n")
 
     menu = get_products()
 
     for thing in menu:
         print(thing['name'], thing['id'])
-
+    print("")
     chooosen_product = select_by_id_or_name(menu, 'product')
 
     arrival_quantity = safe_input('int_positive', 'Recent arrival quantity:')
 
     chooosen_product['quantity'] += arrival_quantity
 
-    print('OKAY. You\'ve registered %s items of %s product' %
+    print('OKAY. You\'ve registered %s items of %s product.' %
           (arrival_quantity, chooosen_product['name']))
-    print('Now there are %s in stock' % chooosen_product['quantity'])
+    print('Now there are %s in stock.' % chooosen_product['quantity'])
     update_products(menu)
