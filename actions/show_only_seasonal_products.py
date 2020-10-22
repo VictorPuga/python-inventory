3288d35f2cbe103db47dbf20c6df36d32a30cf97
VictorPuga <victorpugaruiz@live.com.mx>
2020-10-22 13:10:28 -0600

    documentation and improvements for seasonal_products

--- actions/show_only_seasonal_products.py
+++ actions/show_only_seasonal_products.py
@@ -5,7 +5,12 @@ from utils import (
 
 
 def show_only_seasonal_products():
-    print("--- Show only seasonal products ---")
+    """
+    Ask for the user to select a season. Then, display the products to 
+    the user that match the selected season.
+    """
+
+    print("--- Show only seasonal products ---\n")
 
     menu = get_products()
 
@@ -16,11 +21,16 @@ def show_only_seasonal_products():
         {"id": 4, "name": "FALL"},
         {"id": 5, "name": "WINTER"},
     ]
+
+    print('Select a season:')
     for sea in seasons:
-        print(sea['name'], sea['id'])
+        print(sea['name'].title(), '(' + str(sea['id']) + ')')
+    print()
 
     this_season = select_by_id_or_name(seasons, 'season')
-    print("Products available only in %s season:" % this_season["name"])
+
+    print("Products available only in %s season:" %
+          this_season["name"].title())
     for product in menu:
         if product["season"] == this_season["name"]:
             print("-", product["name"])
