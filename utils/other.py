782467e1ff5e6169e6483dd8885602ec2a3e91a5
VictorPuga <victorpugaruiz@live.com.mx>
2020-10-22 13:09:47 -0600

    documentation for misc utilities

--- utils/other.py
+++ utils/other.py
@@ -2,6 +2,17 @@ import datetime
 
 
 def dict_from_entries(keys, values):
+    """
+    Generate a dictonary from keys and values iterables.
+
+    Parameters:
+    - keys (list or tuple): keys for the dictionary
+    - values (list or tuple): values for the dictionary
+
+    Returns:
+    - new_dict (dictionary)
+    """
+
     new_dict = {}
     for i in range(len(keys)):
         new_dict[keys[i]] = values[i]
@@ -9,6 +20,19 @@ def dict_from_entries(keys, values):
 
 
 def safe_input(input_type, message):
+    """
+    Ask for input.
+
+    Parameters: 
+    - input_type (string): data type for the input. One of:
+        - "string"
+        - "int_positive"
+        - "float_positive"
+
+    Returns:
+    - value (depends on input_type)
+    """
+
     while True:
         value = input(message).strip()
         if value:
@@ -21,12 +45,39 @@ def safe_input(input_type, message):
 
 
 def find_by_key(dict_list, key, value):
+    """
+    Find the first dictionary in an iterable with a key-value pair
+
+    Parameters: 
+        - dict_list (list or tuple): iterable to search
+        - key (string): key to compare
+        - value (any): value to search in key
+
+    Returns: 
+        - element (dictionary): element of dict_list
+        or
+        - None: if no item was found
+    """
+
     for el in dict_list:
         if str(el[key]).upper() == str(value).upper():
             return el
 
 
 def select_by_id_or_name(dict_list, name):
+    """
+    Find an element in an iterable with user input. 
+    Asks for a name or an id, and automatically searches the list.
+    Automatically validates user input.
+
+    Parameters: 
+        - dict_list (list or tuple): iterable to search
+        - name (string): name of the entities in dict_list. Used to provide feedback to the user
+
+    Returns: 
+        - element (dictionary): element of dict_list
+    """
+
     while True:
         selected = None
         name_or_id = safe_input('string', 'Name or Id: ')
@@ -37,6 +88,8 @@ def select_by_id_or_name(dict_list, name):
         else:
             selected = find_by_key(dict_list, 'name', name_or_id)
 
+        # Make sure an element was selected,
+        # otherwise, ask again
         if selected:
             return selected
         else:
@@ -45,11 +98,29 @@ def select_by_id_or_name(dict_list, name):
 
 
 def today():
+    """
+    Get today's date.
+
+    Returns: 
+        - date (string): date formated as "MM/DD/YY"
+    """
+
     date = datetime.datetime.now()
     return date.strftime("%x")
 
 
 def dict_to_csv_line(dictionary, keys):
+    """
+    Transform dictionary into a string to be written in a csv file
+
+    Parameters:
+        - dictionary: object to transform
+        - keys (list or tuple): keys of the dictionary
+
+    Returns:
+        - line (string): csv line
+    """
+
     line = ''
     for k in keys:
         line += str(dictionary[k])
